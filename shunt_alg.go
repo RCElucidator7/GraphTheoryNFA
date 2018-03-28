dd3408b4e548bdc4ef455df904b2121b87f6b3c9
RCElucidator7 <G00332826@gmit.ie>
2018-03-28 20:20:23 +0100

    Most of shunting algorithm complete, need to add special cases

--- shunt_alg.go
+++ shunt_alg.go
@@ -6,7 +6,7 @@ import (
 
 func infixToPostfix(infix string) string {
 	//Assign special characters int values
-	special := map[rune]int{"*": 10 , ".": 9 , "|": 8}
+	special := map[rune]int{'*': 10 , '.': 9 , '|': 8}
 
 
 	//Empty array of runes
@@ -17,15 +17,41 @@ func infixToPostfix(infix string) string {
 	//Loop over string and convert to postfix
 	for _, r := range infix {
 		//range is used to convert type string into type rune
+		switch {
+			case r == '(':
+				stack = append(stack, r)
+			case r == ')':
+				for stack[len(stack)-1] != '('{
+					pFix = append(pFix, stack[len(stack)-1])
 
+					stack = stack[:len(stack)-1]
+				}
+
+				stack = stack[:len(stack)-1]	
+			case special[r] > 0:
+				for len(stack) > 0 && special[r] <= special[stack[len(stack)-1]] {
+					pFix = append(pFix, stack[len(stack)-1])
+
+					stack = stack[:len(stack)-1]
+				}
+
+			default:
+				pFix = append(pFix, r)
+		}
+	}
+	//Takes top element of stack
+	for len(stack) > 0 {
+		pFix = append(pFix, stack[len(stack)-1])
+
+		stack = stack[:len(stack)-1]
 	}
 
 	return string(pFix)
 }
 
 func main() {
-	//Input a.b.c
-	fmt.Println("Infix: a.b.c")
-	fmt.Println("Postfis: ", infixToPostfix("a.b.c"))
+	//Input a.b.c*
+	fmt.Println("Infix: a.b.c*")
+	fmt.Println("Postfis: ", infixToPostfix("a.b.c*"))
 	//Should return ab.c*
 }
