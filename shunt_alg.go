88d39fca0ef57d74c71698e54c07d0e72dfdb9ab
RCElucidator7 <G00332826@gmit.ie>
2018-03-28 17:49:30 +0100

    Project folder and shunting algorithm go file created

---
+++ shunt_alg.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func infixToPostfix(infix string) string {
+	pFix := ""
+
+	return pFix
+}
+
+func main() {
+	//Input a.b.c
+	fmt.Println("Infix: a.b.c")
+	fmt.Println("Postfis: " + infixToPostfix("a.b.c"))
+}
