613eae023d879ea40e531e4131ff2ee8319a435d
RCElucidator7 <G00332826@gmit.ie>
2018-03-28 20:54:29 +0100

    Completed cases for . and |

--- nfa_alg.go
+++ nfa_alg.go
@@ -25,7 +25,39 @@ func regex(postfix string) *NFA {
 
 	//variable for an array of pointers for NFA struct
 	stack := []*NFA{}
+
+	for _, r := postfix {
+		switch r {
+			case '.':
+				fragtwo := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				fragone := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+
+				fragone.accept.edge1 = fragtwo.initial
+
+				stack = append(stack, &NFA{initial: fragone.initial, accept: fragtwo.accept})
+
+			case '|':
+				fragtwo := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				fragone := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+
+				accept := state{}
+				initial := state{edge1: fragone.initial, edge2: fragtwo.initial}
+				fragone.accept.edge1 = &accept
+				fragtwo.accept.edge1 = &accept
+
+				stack = append(stack, &NFA{initial: &initial, accept: &accept})
+
+			case '*':
+
+			default:
+		}
+	}
 	
+	return stack[0]
 }
 
 func main() {
